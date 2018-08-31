8052e729f7effc5945c443a74c1a1e3af0d0bf8c
HishamLY <hishamlazuardi@gmail.com>
2018-08-31 18:09:15 +0700

    add warning dialog

--- NewPermia_Client/PemiraClient/PasswordDialog.Designer.cs
+++ NewPermia_Client/PemiraClient/PasswordDialog.Designer.cs
@@ -36,10 +36,11 @@
             // 
             // input_password
             // 
-            this.input_password.BackColor = System.Drawing.Color.Wheat;
-            this.input_password.Location = new System.Drawing.Point(61, 57);
+            this.input_password.BackColor = System.Drawing.Color.Linen;
+            this.input_password.Location = new System.Drawing.Point(81, 70);
+            this.input_password.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.input_password.Name = "input_password";
-            this.input_password.Size = new System.Drawing.Size(156, 20);
+            this.input_password.Size = new System.Drawing.Size(207, 22);
             this.input_password.TabIndex = 0;
             this.input_password.UseSystemPasswordChar = true;
             this.input_password.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.input_password_KeyPress);
@@ -48,27 +49,32 @@
             // 
             this.label2.AutoSize = true;
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(58, 32);
+            this.label2.Location = new System.Drawing.Point(77, 39);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(130, 13);
+            this.label2.Size = new System.Drawing.Size(171, 17);
             this.label2.TabIndex = 2;
             this.label2.Text = "Password untuk menutup:";
             // 
             // button_submit
             // 
-            this.button_submit.Location = new System.Drawing.Point(61, 86);
+            this.button_submit.BackColor = System.Drawing.Color.Lavender;
+            this.button_submit.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button_submit.Location = new System.Drawing.Point(81, 106);
+            this.button_submit.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button_submit.Name = "button_submit";
-            this.button_submit.Size = new System.Drawing.Size(75, 23);
+            this.button_submit.Size = new System.Drawing.Size(100, 28);
             this.button_submit.TabIndex = 3;
             this.button_submit.Text = "OK";
-            this.button_submit.UseVisualStyleBackColor = true;
+            this.button_submit.UseVisualStyleBackColor = false;
             this.button_submit.Click += new System.EventHandler(this.submit_button_onclick);
             // 
             // button_cancel
             // 
-            this.button_cancel.Location = new System.Drawing.Point(142, 86);
+            this.button_cancel.Location = new System.Drawing.Point(189, 106);
+            this.button_cancel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button_cancel.Name = "button_cancel";
-            this.button_cancel.Size = new System.Drawing.Size(75, 23);
+            this.button_cancel.Size = new System.Drawing.Size(100, 28);
             this.button_cancel.TabIndex = 4;
             this.button_cancel.Text = "Batal";
             this.button_cancel.UseVisualStyleBackColor = true;
@@ -76,15 +82,16 @@
             // 
             // PasswordDialog
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Brown;
-            this.ClientSize = new System.Drawing.Size(272, 148);
+            this.BackColor = System.Drawing.Color.IndianRed;
+            this.ClientSize = new System.Drawing.Size(363, 182);
             this.Controls.Add(this.button_cancel);
             this.Controls.Add(this.button_submit);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.input_password);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "PasswordDialog";
             this.Text = "Form1";
             this.ResumeLayout(false);
--- NewPermia_Client/PemiraClient/RedoWarning.Designer.cs
+++ NewPermia_Client/PemiraClient/RedoWarning.Designer.cs
@@ -37,17 +37,19 @@
             this.label_redo_warning.AutoSize = true;
             this.label_redo_warning.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label_redo_warning.ForeColor = System.Drawing.Color.White;
-            this.label_redo_warning.Location = new System.Drawing.Point(30, 24);
+            this.label_redo_warning.Location = new System.Drawing.Point(40, 30);
+            this.label_redo_warning.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label_redo_warning.Name = "label_redo_warning";
-            this.label_redo_warning.Size = new System.Drawing.Size(450, 20);
+            this.label_redo_warning.Size = new System.Drawing.Size(558, 25);
             this.label_redo_warning.TabIndex = 0;
             this.label_redo_warning.Text = "Anda hanya dapat melakukan pemilihan ulang sebanyak 2 kali.";
             // 
             // button_ok_redo_warning
             // 
-            this.button_ok_redo_warning.Location = new System.Drawing.Point(220, 67);
+            this.button_ok_redo_warning.Location = new System.Drawing.Point(293, 82);
+            this.button_ok_redo_warning.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button_ok_redo_warning.Name = "button_ok_redo_warning";
-            this.button_ok_redo_warning.Size = new System.Drawing.Size(75, 23);
+            this.button_ok_redo_warning.Size = new System.Drawing.Size(100, 28);
             this.button_ok_redo_warning.TabIndex = 1;
             this.button_ok_redo_warning.Text = "OK";
             this.button_ok_redo_warning.UseVisualStyleBackColor = true;
@@ -55,13 +57,14 @@
             // 
             // RedoWarning
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Brown;
-            this.ClientSize = new System.Drawing.Size(509, 120);
+            this.BackColor = System.Drawing.Color.IndianRed;
+            this.ClientSize = new System.Drawing.Size(679, 148);
             this.Controls.Add(this.button_ok_redo_warning);
             this.Controls.Add(this.label_redo_warning);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "RedoWarning";
             this.Text = "RedoWarning";
             this.ResumeLayout(false);
